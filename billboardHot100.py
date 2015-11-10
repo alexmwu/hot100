650d305ebf309c2f0fac2f65ec4ccb92398e68b9
Alex Wu <mwumbojumbo@gmail.com>
2015-11-09 21:02:21 -0500

    Remove unneccessary comments/code in script

--- billboardHot100.py
+++ billboardHot100.py
@@ -9,49 +9,30 @@ import urllib2
 from datetime import date
 from bs4 import BeautifulSoup
 
-fp = open("hot100.csv","w")
-
 bbSite = 'http://billboardtop100of.com/'
 
-startYear = 1940
+# well formatted tables start at 1945
+startYear = 1945
 
 currYear = date.today().year
 # iterate to the current year - 1
 for y in xrange(startYear, currYear):
-    pass
-y = startYear
-# for whatever reason, the link to 1940 is 336
-if y == 1940:
-    y = 336
-response = urllib2.urlopen(bbSite + str(y) + '-2/')
-html = response.read()
-soup = BeautifulSoup(html, 'html.parser')
+    fp = open('data/' + str(y) + 'hot100.csv','w')
+
+    y = startYear
+
+    response = urllib2.urlopen(bbSite + str(y) + '-2/')
+    html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
 # 3 cases: no table, just list of hot 100 hits, table with
 # cells with <a> tags, and cell without <a> tags
-if soup.table:
-    rows = soup.table.findChildren(['tr'])
-    for row in rows:
-        cells = row.findChildren('td')
-        out_line = []
-        for cell in cells:
-# check if the td has an <a> tag
-            links = cell.findChildren('a')
-            if len(links) == 0:
+    if soup.table:
+        rows = soup.table.findChildren(['tr'])
+        for row in rows:
+            cells = row.findChildren('td')
+            out_line = []
+            for cell in cells:
                 out_line.append(cell.text)
-            else:
-                for link in links:
-                    out_line.append(link.text)
-        out_str = ','.join(out_line).encode('ascii', 'ignore')
-        fp.write(out_str + '\n')
-# no table (only seen in 1940)
-
-fp.close()
-
-    # for tr in soup.table:
-# # handle cells with <a> tags
-        # links = soup.findChildren('a')
-        # if len(links) == 0:
-            # print tr
-        # else:
-            # for link in links:
-                # print link
+            out_str = ','.join(out_line).encode('ascii', 'ignore')
+            fp.write(out_str + '\n')
+    fp.close()
