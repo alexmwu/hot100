ce9dd2e7b0a76a1f179caa8494d240f7b2168749
Kim Ngo <kngo@nd.edu>
2015-12-11 13:23:39 -0500

    ran random forest classifer on two decades to see how accuracy changes the closer the decades are. results are in data/random_forest_classifier

--- randomForest.py
+++ randomForest.py
@@ -11,6 +11,7 @@
 import pandas
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 # user functions
 from bagOfWords import testAccuracy
@@ -19,7 +20,9 @@ from bagOfWords import testAccuracy
 from baseClassifier import *
 
 CV = 10
-OUTPUT_PATH = 'data/random_forest_classifer_output.csv'
+OUTPUT_PATH = 'data/random_forest_classifier/output.csv'
+CV_PLOT_PATH = 'data/random_forest_classifier/cross_validation.png'
+TEST_PLOT_PATH = 'data/random_forest_classifier/test_prediction.png'
 
 # Fit model and learn vocabulary on existing lyrics
 ### Random Forest Classifier ###
@@ -27,9 +30,17 @@ OUTPUT_PATH = 'data/random_forest_classifer_output.csv'
 forest = RandomForestClassifier(n_estimators=100)
 
 # Cross validation on training data
-train_predict = cross_validation.cross_val_predict(forest, trainDataFeatures, y=trainDFNotNull['DECADE'], cv=CV)
-print 'Training Accuracy:', '%.2f' % metrics.accuracy_score(trainDFNotNull['DECADE'], train_predict) + '%'
+train_predicted = cross_validation.cross_val_predict(forest, trainDataFeatures, y=trainDFNotNull['DECADE'], cv=CV)
+score = metrics.accuracy_score(trainDFNotNull['DECADE'], train_predicted)
+print 'Training Accuracy:', str(score*100) + '%'
 
+fig, ax = plt.subplots()
+ax.scatter(trainDFNotNull['YEAR'], train_predicted)
+ax.plot([trainDFNotNull['DECADE'].min(), trainDFNotNull['DECADE'].max()], [trainDFNotNull['DECADE'].min(), trainDFNotNull['DECADE'].max()])
+#ax.set_title(str(CV)+'-Fold Cross Validation')
+ax.set_xlabel('Measured Year')
+ax.set_ylabel('Predicted Decade')
+plt.savefig(CV_PLOT_PATH, bbox_inches='tight')
 
 # Fit forest model to training set using bag of words as features and decade as label
 forest = forest.fit(trainDataFeatures, trainDFNotNull['DECADE'])
@@ -50,3 +61,10 @@ output.to_csv(OUTPUT_PATH, index=False, sep='@', quoting=3, \
         columns=['NUM','ARIST', 'SONG', 'YEAR', 'DECADE'])
 
 print 'Testing Accuracy: ' + '%.2f' % testAccuracy(output) + '%'
+
+ax.scatter(testDFNotNull['YEAR'], result)
+ax.plot([testDFNotNull['DECADE'].min(), testDFNotNull['DECADE'].max()], [testDFNotNull['DECADE'].min(), testDFNotNull['DECADE'].max()])
+#ax.set_title('Test Prediction')
+ax.set_xlabel('Measured Year')
+ax.set_ylabel('Predicted Decade')
+plt.savefig(TEST_PLOT_PATH, bbox_inches='tight')
