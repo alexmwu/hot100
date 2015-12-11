dfa66356c8a84d586950564a696a6e4a7b6f2796
Kim Ngo <kngo@nd.edu>
2015-12-11 12:11:56 -0500

    Modified baseclassifer to split full dataset into train and testing. Random forest now does 10-fold cross validation on training data set.

--- randomForest.py
+++ randomForest.py
@@ -10,22 +10,33 @@
 
 import pandas
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
 
 # user functions
 from bagOfWords import testAccuracy
+
 # run and import all variables in baseClassifier
 from baseClassifier import *
+
+CV = 10
+OUTPUT_PATH = 'data/random_forest_classifer_output.csv'
 
 # Fit model and learn vocabulary on existing lyrics
 ### Random Forest Classifier ###
 # Initialize random forest with 100 trees
 forest = RandomForestClassifier(n_estimators=100)
 
+# Cross validation on training data
+train_predict = cross_validation.cross_val_predict(forest, trainDataFeatures, y=trainDFNotNull['DECADE'], cv=CV)
+print 'Training Accuracy:', '%.2f' % metrics.accuracy_score(trainDFNotNull['DECADE'], train_predict) + '%'
+
+
 # Fit forest model to training set using bag of words as features and decade as label
 forest = forest.fit(trainDataFeatures, trainDFNotNull['DECADE'])
 
 # Use random forest to make decade label predictions
 result = forest.predict(testDataFeatures)
+
 # Copy results to pandas DF with predicted 'DECADE' column
 output = pandas.DataFrame(data = {	\
 	'NUM':testDFNotNull['NUM'],				\
@@ -38,5 +49,4 @@ output = pandas.DataFrame(data = {	\
 output.to_csv(OUTPUT_PATH, index=False, sep='@', quoting=3, \
         columns=['NUM','ARIST', 'SONG', 'YEAR', 'DECADE'])
 
-print 'Accuracy: ' + '%.2f' % testAccuracy(output) + '%'
-
+print 'Testing Accuracy: ' + '%.2f' % testAccuracy(output) + '%'
